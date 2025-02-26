acf26e09ac05735433ccc5f5ffde6a67e2b8b935
van-cpu <ecrowl01@student.ubc.ca>
2025-02-26 12:12:17 -0800

    Added Css functionality to the bookmark

--- Frontend/js/job-details.js
+++ Frontend/js/job-details.js
@@ -1,13 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
 
-let book = document.getElementById("bookmark");
+    let book = document.getElementById("bookmark");
+    let clicked = false;
+    book.addEventListener("click", function () { 
+        
 
-book.addEventListener("click",function(){
+        book.src = "/Frontend/photos/bookmarkClick.png"
+        
+        
+        //book.style.transform = "scale(1)";
 
-    book.classList.add("bookClick");
-    book.src = "/Frontend/photos/defaultimage.png"
-
-});
+        
+        if (clicked) {
+            book.src = "/Frontend/photos/bookmark.png"; 
+            book.style.transition = "transform 4.3s ease";
+            book.style.transform = "scale(1)";
+            clicked = false;
+        } else {
+            book.src = "/Frontend/photos/bookmarkClick.png";
+            book.style.transition = "transform 0.3s ease";
+            book.style.transform = "scale(1.8)";
+            clicked = true;
+        }
+       
+    });
 
 });
