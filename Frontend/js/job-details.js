c3f8f122be99ced2d3a25c3cd5f4ae20be5b8fed
van-cpu <ecrowl01@student.ubc.ca>
2025-02-27 12:44:18 -0800

    added link to apply button

--- Frontend/js/job-details.js
+++ Frontend/js/job-details.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let book = document.getElementById("bookmark");
     let clicked = false;
-    book.addEventListener("click", function () { 
-        
+    book.addEventListener("click", function () {
+
 
         book.src = "/Frontend/photos/bookmarkClick.png"
-        
-        
+
+
         //book.style.transform = "scale(1)";
 
-        
+
         if (clicked) {
-            book.src = "/Frontend/photos/bookmark.png"; 
+            book.src = "/Frontend/photos/bookmark.png";
             book.style.transition = "transform 4.3s ease";
             book.style.transform = "scale(1)";
             clicked = false;
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
             book.style.transform = "scale(1.8)";
             clicked = true;
         }
-       
+
     });
 
 });
