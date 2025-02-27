17718e6ca22d3b4721ab8d89e0a35d3514dafca3
van-cpu <ecrowl01@student.ubc.ca>
2025-02-27 14:02:30 -0800

    Added Report Form Html

--- Frontend/js/job-details.js
+++ Frontend/js/job-details.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     let book = document.getElementById("bookmark");
+    let report = document.getElementById("report");
     let clicked = false;
+    let clickedReport = false;
     book.addEventListener("click", function () {
 
 
         book.src = "/Frontend/photos/bookmarkClick.png"
-
-
-        //book.style.transform = "scale(1)";
 
 
         if (clicked) {
@@ -25,5 +24,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     });
+
+    report.addEventListener("click", function () {
+
+
+        report.src = "/Frontend/photos/bookmarkClick.png"
+
+
+        if (clickedReport) {
+            report.src = "/Frontend/photos/report.png";
+            report.style.transition = "transform 4.3s ease";
+            report.style.transform = "scale(1)";
+            clickedReport = false;
+        } else {
+            report.src = "/Frontend/photos/reportClick.png";
+            report.style.transition = "transform 0.3s ease";
+            report.style.transform = "scale(1.8)";
+            clickedReport = true;
+        }
+
+    });
+
+
 
 });
