7e60407589a81c226faed62b2193fc0dd424cc1c
van-cpu <ecrowl01@student.ubc.ca>
2025-02-27 20:12:33 -0800

    Added issue popup menu

--- Frontend/js/admin.js
+++ Frontend/js/admin.js
@@ -3,23 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
     let checkboxes = document.querySelectorAll(".filterCheckbox");
     let issueList = document.getElementById("reportedIssues").children;
     let issueCount = document.getElementById("issueCount");
+    let viewReportedIssue = document.getElementById("viewReportedIssue");
+
+    
+
+    document.querySelectorAll(".viewProb").forEach((button) => {
+        button.addEventListener("click", function () {
+
+            viewReportedIssue.style.display = "block";
+        });
+    });
+
 
 
     issueCount.innerText = issueList.length;
+
     function filterIssues() {
         let activeFilters = [];
-    
+
 
         for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked) {
                 activeFilters.push(checkboxes[i].value);
             }
         }
-    
+
 
         for (let i = 0; i < issueList.length; i++) {
             let issueCategory = issueList[i].getAttribute("data-category");
-    
+
             if (activeFilters.indexOf(issueCategory) !== -1) {
                 issueList[i].style.display = "list-item";
             } else {
@@ -27,12 +39,12 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     }
-    
+
 
     for (let i = 0; i < checkboxes.length; i++) {
         checkboxes[i].addEventListener("change", filterIssues);
     }
-    
+
     filterIssues();
 
 });
