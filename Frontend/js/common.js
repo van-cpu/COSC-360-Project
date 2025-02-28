4b5025aae022d956c665b039a230c74eb325126a
van-cpu <ecrowl01@student.ubc.ca>
2025-02-28 08:51:07 -0800

    Created Basic Universal Navbar

--- Frontend/js/common.js
+++ Frontend/js/common.js
@@ -1,1 +1,14 @@
 /*Universal JS*/
+document.addEventListener("DOMContentLoaded", function(){
+    
+document.body.innerHTML += `
+<nav id="navbar">
+    <a href="#" id="logo">MySite</a>
+    <ul id="nav-links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="job-listings.html">Job listings</a></li>
+        <li><a href="profile.html">Profile</a></li>
+        <li><a href="admin.html">Admin Panel</a></li>
+    </ul>
+</nav>
+`;});
